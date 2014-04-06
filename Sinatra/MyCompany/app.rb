760a9ba222c52d01f8b8d2b4ec9432f1336995ad
ajlopez <webmaster@ajlopez.com>
2014-04-06 16:48:03 -0300

    MyCompany Sinatra sample, WIP

--- Sinatra/MyCompany/app.rb
+++ Sinatra/MyCompany/app.rb
@@ -1,7 +1,8 @@
 # myapp.rb
 
 require 'sinatra'
-require 'dm-core'
+require 'sinatra/reloader' if development?
+require 'data_mapper'
 
 require './entities/customer'
 require './entities/supplier'
@@ -10,9 +11,36 @@ DataMapper.finalize
 
 DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
 
+Customer.auto_upgrade!
+Supplier.auto_upgrade!
+
 get '/' do
-    @title = 'My Company'
-    erb :index
+  @title = 'My Company'
+  erb :index
 end
 
+get '/customer' do
+  @title = 'Customer List'
+  @customers = Customer.all
+  erb :customerlist
+end
+
+get '/customer/new' do
+  @title = 'New Customer'
+  erb :customernew
+end
+
+post '/customer/new' do
+  customer = Customer.new
+  customer.name = params[:name]
+  customer.address = params[:address]
+  customer.save
+  redirect to('/customer')
+end
+
+get '/customer/:id' do
+  @title = 'Customer'
+  @customer = Customer.get(params[:id])
+  erb :customerview
+end
 
