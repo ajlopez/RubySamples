9fa18ad337b0ee3b0361d5f9e45cf0d98795b363
ajlopez <webmaster@ajlopez.com>
2014-04-06 12:05:50 -0300

    MyCompany app.rb WIP

--- Sinatra/MyCompany/app.rb
+++ Sinatra/MyCompany/app.rb
@@ -1,8 +1,18 @@
 # myapp.rb
 
 require 'sinatra'
+require 'dm-core'
+
+require './entities/customer'
+require './entities/supplier'
+
+DataMapper.finalize
+
+DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
 
 get '/' do
     @title = 'My Company'
     erb :index
 end
+
+
