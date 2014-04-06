d6601af727790f29b31f8967581c62e53bfcdf06
ajlopez <webmaster@ajlopez.com>
2014-04-06 12:30:03 -0300

    Customer list, WIP

--- Sinatra/MyCompany/app.rb
+++ Sinatra/MyCompany/app.rb
@@ -2,6 +2,7 @@
 
 require 'sinatra'
 require 'dm-core'
+require 'dm-migrations'
 
 require './entities/customer'
 require './entities/supplier'
@@ -10,9 +11,22 @@ DataMapper.finalize
 
 DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
 
+# Customer.auto_migrate!
+# Supplier.auto_migrate!
+
 get '/' do
     @title = 'My Company'
     erb :index
 end
 
+get '/customer' do
+    @title = 'Customer List'
+    @customers = Customer.all
+    erb :customerlist
+end
 
+get '/supplier' do
+    @title = 'Supplier List'
+    @suppliers = Supplier.all
+    erb :supplierlist
+end
